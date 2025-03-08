e6b79727a53138f246e7e62d12b43c6e2e1cdd59
sharibkhan1 <sharibkhan939@gmail.com>
2025-03-08 23:27:21 +0530

    bg

--- src/app/(portfoliodetail)/layout.tsx
+++ src/app/(portfoliodetail)/layout.tsx
@@ -21,6 +21,8 @@ const LayoutPage = ({ children }: { children: ReactNode }) => {
 
       {/* Content */}
       <div className=" mt-[40%] md:mt-28 relative w-full">
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[60vw] h-[50vh] md:w-[70vw] md:h-[70vh] bg-gradient-to-t from-[#3d2e25] via-[#7a5e47] to-[#f7f4f1] rounded-full blur-3xl opacity-70"></div>
+
           {children}
           </div>
     </div>
